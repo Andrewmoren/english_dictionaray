ded9048a403a9288b21a8cee85e433d3c2be72e4
Andrew <andrewmoren93@gmail.com>
2023-10-31 23:57:30 +0300

    step 1.1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ const App = () => {
       <form>
         <div className="row">
           <textarea name="" id="" cols="30" rows="4" placeholder="Enter text" />
+          <div className="voices-icons">
+            <div className="select-voices">
+              <select name="" id="">
+                <option value="">English</option>
+                <option value="">English</option>
+                <option value="">English</option>
+              </select>
+            </div>
+
+            <i class="fa-solid fa-volume-high" />
+          </div>
         </div>
       </form>
     </div>
